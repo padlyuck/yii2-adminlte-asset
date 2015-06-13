88afd85d7f19d9e268ffe4f1b7af6a7eea15f035
Vladimir Siritsa <padlyuck@gmail.com>
2015-06-13 16:09:31 +0300

    Update AdminLteAsset.php

--- web/AdminLteAsset.php
+++ web/AdminLteAsset.php
@@ -16,8 +16,8 @@ use yii\web\AssetBundle;
 class AdminLteAsset extends AssetBundle
 {
     public $sourcePath = '@bower/';
-    public $css = ['admin-lte/css/AdminLTE.css', 'font-awesome/css/font-awesome.min.css'];
-    public $js = ['admin-lte/js/AdminLTE/app.js'];
+    public $css = ['adminlte/css/AdminLTE.css', 'font-awesome/css/font-awesome.min.css'];
+    public $js = ['adminlte/js/AdminLTE/app.js'];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
